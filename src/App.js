2a8bbcf066ee6087aa1272185c093972e18dcd02
Thiago1223 <tifreitas10@gmail.com>
2023-09-03 23:19:17 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
+
+function App() {
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route exact path='/empresa' element={<Empresa />}></Route>
+        <Route exact path='/contato' element={<Contato />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App;
---
+++ src/components/Evento.js
@@ -0,0 +1,23 @@
+import Button from "./evento/Button"
+
+function Evento({numero}) {
+
+    function meuEvento() {
+        console.log(`Ativando primeiro evento!`)
+    }
+
+    function segundoEvento() {
+        console.log(`Ativando o segundo evento!`)
+    }
+
+    return (
+        <div>
+            <p>Clique para dispara um evento:</p>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo Evento" />
+        </div>
+    )
+
+}
+
+export default Evento
